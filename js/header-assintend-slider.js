9d447b099796604f70c3941b89f79ed596b57774
maksym davydenko <Maksym_Davydenko@epam.com>
2020-07-28 23:05:36 +0300

    add margin and paddings

--- js/header-assintend-slider.js
+++ js/header-assintend-slider.js
@@ -36,144 +36,17 @@ $(document).ready(function () {
 });
 
 
-// $('.sliderSlick').on('init', function (slick) {
-// var active = $('.sliderSlick .slick-active');
-// var first = active.eq(0);
-// var last = active.eq(active.length - 1);
-// first.addClass('test');
-// last.addClass('test');
-// });
-
-// $('.sliderSlick').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-//     var active = $('.sliderSlick .slick-active');
-//     var first = active.eq(0);
-//     var last = active.eq(active.length - 1);
-//     active.removeClass('test')
-//     first.addClass('test');
-//     last.addClass('test');
-// });
-
-
-// let slider = 1;
-
-
 //suctom buttons
 
 $('.js-header-slide-prev').on('click', function () {
     $('.header-assistent-slider').slick('slickPrev');
-
-    // var num = $('.item-num-slide');
-    // num.removeClass('item-num-slide_active');
-
-    // slider = slider >= num.length ? 0 : slider;
-    // slider++;
-
-    // num[slider - 1].classList.add('item-num-slide_active');
-
-
-    // console.log(slider)
-
-
 });
 
 
 $('.js-header-slide-next').on('click', function () {
     $('.header-assistent-slider').slick('slickNext');
-    // var num = $('.item-num-slide');
-    // num.removeClass('item-num-slide_active');
-
-    // slider = slider <= 0 ? num.length : slider;
-    // slider--;
-
-
-    // num[slider].classList.add('item-num-slide_active');
-
-    // console.log(slider)
-
-    // curSlider.addClass('item-num-slide_active');
-    // var active = $('.sliderSlick .slick-active');
-    // var first = active.eq(0);
-    // var last = active.eq(active.length - 1);
-    // active.removeClass('test')
-    // first.addClass('test');
-    // last.addClass('test');
 
 });
 
 
-// $('.sliderSlick').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-//     var active = $('.slick-active');
-
-// var slickSlide = $('.slick-slider');
-// var first = active.eq(0);
-// var last = active.eq(active.length - 1);
-
-// active.removeClass('test');
-// // slickSlide.removeClass('curSlider');
-
-
-// var slides = document.querySelectorAll('.slick-slide');
-
-// slides.forEach(slider => {
-//     slider.classList.remove('curSlider');
-// })
-
-// var curSlide = document.querySelector('.slick-current');
-// curSlide.classList.add('curSlider');
-
-// first.addClass('test');
-// last.addClass('test');
-// var curSlide = $('.curSlide');
-// curSlide.removeClass('curSlide');
-
-
-// });
-
-
-// $('.universities__slider').on('init', function (slick) {
-//     var active = $('.universities__slider .slick-active');
-//     var first = active.eq(0);
-//     var last = active.eq(active.length - 1);
-//     first.addClass('opacity');
-//     last.addClass('opacity');
-// });
-
-// $('.universities__slider').on('init', function (slick) {
-//     var active = $('.universities__slider .slick-active');
-//     var first = active.eq(0);
-//     var last = active.eq(active.length - 1);
-//     first.addClass('test');
-//     last.addClass('test');
-// });
-
-// $('.universities__slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-//     var active = $('.slick-active');
-
-//     var first = active.eq(0);
-//     var last = active.eq(active.length - 1);
-
-//     active.removeClass('opacity');
-
-//     first.addClass('opacity');
-//     last.addClass('opacity');
-
-// });
-
-
-// $('.gallery__slider').on('init', function (slick) {
-//     var active = $('.gallery__slider .slick-active');
-//     var first = active.eq(0);
-//     var last = active.eq(active.length - 1);
-//     first.addClass('opacity');
-//     last.addClass('opacity');
-// });
-
-// $('.gallery__slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-//     var active = $('.gallery__slider .slick-active');
-//     var first = active.eq(0);
-//     var last = active.eq(active.length - 1);
-//     active.removeClass('opacity')
-//     first.addClass('opacity');
-//     last.addClass('opacity');
-// });
 
