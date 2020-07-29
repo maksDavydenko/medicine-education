97ca27c7ad74e4a1dda8aee9add9e19e32e288b6
maksym davydenko <Maksym_Davydenko@epam.com>
2020-07-29 22:22:57 +0300

    finish marcup

--- js/header-assintend-slider.js
+++ js/header-assintend-slider.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.header-assistent-slider').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         arrows: false,
         speed: 500,
         // adaptiveHeight: true,
@@ -40,13 +40,58 @@ $(document).ready(function () {
 
 $('.js-header-slide-prev').on('click', function () {
     $('.header-assistent-slider').slick('slickPrev');
+
+
+
 });
 
 
 $('.js-header-slide-next').on('click', function () {
     $('.header-assistent-slider').slick('slickNext');
+    changeSlide();
 
 });
 
+let nums = $('.header-slider__nav-item');
+let curSlide = 1;
 
+document.querySelectorAll('.header-slider__nav-item')
+
+nums = Array.from(nums);
+
+
+function changeSlide() {
+
+    let activeIndex = null;
+
+    nums.forEach((element, index) => {
+        if (element.classList.contains('header-slider__nav-item--active')) {
+            activeIndex = index + 1;
+        }
+
+        element.classList.remove('header-slider__nav-item--active');
+
+        console.log(element.classList);
+
+
+    });
+
+    console.log(activeIndex)
+
+    let indexUp = 1;
+    let indexDown = 1;
+
+    for (let i = activeIndex; i < nums.length; i++) {
+        nums[i].style.opacity = 1 - indexUp * 0.2;
+        indexUp++
+    }
+
+    for (let i = activeIndex; i > 0; i--) {
+        nums[i].style.opacity = 1 - indexDown * 0.2;
+        indexDown++
+    }
+
+    nums[activeIndex + 1].classList.add('header-slider__nav-item--active')
+
+}
 
