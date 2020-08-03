48f169a4fc024862b75ddba2238831a3d29af0ee
maksym davydenko <Maksym_Davydenko@epam.com>
2020-08-03 18:43:11 +0300

    fix img size

--- js/slliderData.js
+++ js/slliderData.js
@@ -1,37 +1,37 @@
 const data = [
     {
-        img: 'url(../img/photo/headerSlider/medicine.png)',
+        img: 'url(./../img/photo/headerSlider/medicine.png)',
 
         link: null,
         title: 'MEDICINE',
         info: 'The best thing about studying MBBS in Russia that there is no entrance test for admission in Russian medical colleges/universities. The degrees have been validated by WHO and UNESCO',
     },
     {
-        img: 'url(../img/photo/headerSlider/business.png)',
+        img: 'url(./../img/photo/headerSlider/business.png)',
         link: null,
         title: 'busines',
         info: 'The best thing about studying MBBS in Russia that there is no entrance test for admission in Russian medical colleges/universities. The degrees have been validated by WHO and UNESCO',
     },
     {
-        img: 'url(../img/photo/headerSlider/computer.png)',
+        img: 'url(./../img/photo/headerSlider/computer.png)',
         link: null,
         title: 'Computer',
         info: 'The best thing about studying computer-related courses in Russia is that there are no entrance tests or exams to become admitted to Russian and Ukrainian medical universities. The degrees received at our universities are recognized both on the local and international level, and students can easily become employed after graduation.',
     },
     {
-        img: 'url(../img/photo/headerSlider/engineering.png)',
+        img: 'url(./../img/photo/headerSlider/engineering.png)',
         link: null,
         title: 'Engineering',
         info: 'The best thing about studying MBBS in Russia that there is no entrance test for admission in Russian medical colleges/universities. The degrees have been validated by WHO and UNESCO',
     },
     {
-        img: 'url(../img/photo/headerSlider/aviation.png)',
+        img: 'url(./../img/photo/headerSlider/aviation.png)',
         link: null,
         title: 'Aviation',
         info: 'The best thing about studying MBBS in Russia that there is no entrance test for admission in Russian medical colleges/universities. The degrees have been validated by WHO and UNESCO',
     },
     {
-        img: 'url(../img/photo/headerSlider/other-courses.png)',
+        img: 'url(./../img/photo/headerSlider/other-courses.png)',
         link: null,
         title: 'Other ',
         info: 'The best thing about studying MBBS in Russia that there is no entrance test for admission in Russian medical colleges/universities. The degrees have been validated by WHO and UNESCO',
