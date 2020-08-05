2a7da743dc4fc552facdb4a0144c8afb7cbde083
maksym davydenko <Maksym_Davydenko@epam.com>
2020-08-06 00:57:42 +0300

    fix app btn

--- js/headerSticky.js
+++ js/headerSticky.js
@@ -1,3 +1,23 @@
+// const applyBtn = document.querySelector('.connection-info__btn--sticky');
+// var $element = $('.footer');
+// let counter = 0;
+// $(window).scroll(function () {
+//     var scroll = $(window).scrollTop() + $(window).height();
+//     //Если скролл до конца елемента
+//     //var offset = $element.offset().top + $element.height();
+//     //Если скролл до начала елемента
+//     var offset = $element.offset().top
+
+//     if (scroll > offset && counter == 0) {
+//         $('.connection-info__btn--sticky').fadeIn(500);
+//         console.log('rest')
+//         counter = 1;
+//     }
+// });
+// $('.btn').click(function(){
+// $('#block').slideUp();
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
     const headerWrap = document.querySelector('.header');
     const upBtn = document.querySelector('.upBtn');
@@ -16,9 +36,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
         }
     });
+    var $element = $('.footer');
+    let counter = 0;
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop() + $(window).height();
+        //Если скролл до конца елемента
+        //var offset = $element.offset().top + $element.height();
+        //Если скролл до начала елемента
+        var offset = $element.offset().top
+
+        if (scroll > offset - 80 && counter == 0) {
+            $('.upBtn').css('position', 'static');
+            $('.upBtn').css('transform', 'translate(0, 0)');
+
+            console.log('rest')
+            // counter = 1;
+        }
+
+        if (scroll < offset - 80 && counter == 0) {
+            $('.upBtn').css('position', 'fixed');
+            $('.upBtn').css('transform', 'translate(-50%, 0)');
+
+
+            // counter = 1;
+        }
+    });
+
+
+
 
 })
-
 
 
 
