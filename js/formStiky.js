171c3e0a22ea6fc1b6e662d6675e3a6ea5e4c99b
maksym davydenko <Maksym_Davydenko@epam.com>
2020-08-30 01:31:25 +0300

    adaptive assistant page

--- js/formStiky.js
+++ js/formStiky.js
@@ -15,14 +15,13 @@ $(window).resize(function () {
     console.log(screenSize)
 })
 
-if (screenSize > 1000) {
+if (screenSize > 800) {
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         var offsetAbout = $elementAbout.offset().top
         var offsetFooter = $elementFooter.offset().top;
         var offsercheckUniverse = $checkUniverse.offset().top;
 
-        // console.log($(window).scrollTop())
         if (scroll < offsetAbout + $('.quick-navigation').height()) {
             $('.about-form').css('position', 'static');
             $('.about-form').css('margin-top', '0');
@@ -31,7 +30,7 @@ if (screenSize > 1000) {
         if (scroll > offsetAbout + $('.quick-navigation').height()) {
             $('.about-form').css('position', 'absolute');
             $('.comment-form').css('transition', '0.01s');
-            $('.about-form').css('top', $(window).scrollTop() - $('.about-form').height() + 50);
+            $('.about-form').css('top', $(window).scrollTop() - $('.about-form').height() + 100);
 
         }
 
@@ -40,33 +39,29 @@ if (screenSize > 1000) {
             $('.about-form').css('margin-top', 'auto');
         }
 
-
-
         // comment-form
         //
 
-        if (scroll < offsercheckUniverse) {
+        if (scroll < offsercheckUniverse + 150) {
             $('.comment-form').css('position', 'static');
             $('.comment-form').css('margin-top', '0');
+            $('.comment-form').css('width', '100%');
         }
 
         if (scroll > offsercheckUniverse) {
             $('.comment-form').css('position', 'absolute');
             $('.comment-form').css('right', '0');
+            $('.comment-form').css('width', ' 33%');
             $('.comment-form').css('transition', '0.01s');
-            $('.comment-form').css('top', $(window).scrollTop() - offsercheckUniverse - 100);
+            $('.comment-form').css('top', $(window).scrollTop() - offsercheckUniverse - 50);
 
         }
 
         if (scroll > offsetFooter - $('.comment-form').height() - 300) {
             $('.comment-form').css('position', 'static');
+            $('.comment-form').css('width', '100%');
             $('.comment-form').css('margin-top', 'auto');
         }
-
-
-
     });
-
-
 }
 
