3ae3e970f24f54eea914e6367b51a4d1d3d88820
maksym davydenko <Maksym_Davydenko@epam.com>
2020-09-04 01:19:16 +0300

    add menu scroll function

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,10 @@ const menu = document.getElementById('menu');
 const header = document.querySelector('header');
 const headerSlider = document.querySelector('.header-slider');
 const adaptiveMenu = document.querySelector('.adaptive-menu');
+const aboutSection = document.querySelector('.about-section');
 
+
+let menuOpen = false;
 document.addEventListener('click', (e) => {
     const target = e.target;
     const menuWrap = target.closest(".adaptive-menu");
@@ -11,24 +14,52 @@ document.addEventListener('click', (e) => {
         header.style.position = 'fixed';
         adaptiveMenu.style.display = 'none';
         headerSlider.style.marginTop = '52px';
+        $('body').css('height', 'auto');
+        $('body').css('overflow', 'auto');
+        menuOpen = false;
     }
 })
 
 menu.addEventListener('click', () => {
+
+
     if (adaptiveMenu.style.display === 'block') {
         header.style.position = 'fixed';
         adaptiveMenu.style.display = 'none';
         headerSlider.style.marginTop = '52px';
-
+        $('body').css('height', 'auto');
+        $('body').css('overflow', 'auto');
+        menuOpen = false
 
     } else {
         adaptiveMenu.style.display = 'block';
         header.style.position = 'static';
         headerSlider.style.marginTop = '0';
         window.scrollTo(0, 0);
+        menuOpen = true;
+        let $element = $('.about-section');
+
+        $(window).scroll(function () {
+            let scroll = $(window).scrollTop() + $(window).height();
+            let offset = $element.offset().top
+
+            // if (scroll > offset) {
+
+            // }
+            scrollEvent(scroll > offset)
+        });
+        function scrollEvent(scrollEvent) {
+            if (scrollEvent && menuOpen) {
+                $('body').css('height', `${$element.offset().top}px`);
+                $('body').css('overflow', 'hidden');
+            }
+        }
+
     }
 
 })
+
+
 
 $('details').click(function (event) {
     $('details').not(this).removeAttr("open");
